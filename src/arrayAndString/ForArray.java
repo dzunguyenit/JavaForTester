3d2546c12754f0296fa921143fb677c07d1b19ab
admin <dzunguyenit@gmail.com>
2019-02-16 17:42:02 +0700

    up

---
+++ src/arrayAndString/ForArray.java
@@ -0,0 +1,13 @@
+package arrayAndString;
+
+public class ForArray {
+	public static void main(String args[]) {
+		int[] myArray = { 1, 2, 5, 4, 0 };
+
+		// Print all the array elements
+		for (int i = 0; i < myArray.length; i++) {
+			System.out.println(myArray[i] + " ");
+		}
+
+	}
+}
---
+++ src/arrayAndString/ForEachArray.java
@@ -0,0 +1,13 @@
+package arrayAndString;
+
+public class ForEachArray {
+	public static void main(String args[]) {
+		int[] myArray = { 1, 2, 5, 4, 0 };
+
+		// Print all the array elements
+		for (int x : myArray) {
+			System.out.println(x);
+		}
+
+	}
+}
