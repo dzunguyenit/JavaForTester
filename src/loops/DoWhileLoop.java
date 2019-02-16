9c192717899072ca17990800597843f693e68855
admin <dzunguyenit@gmail.com>
2019-02-16 16:58:50 +0700

    up

---
+++ src/loops/DoWhileLoop.java
@@ -0,0 +1,14 @@
+package loops;
+
+public class DoWhileLoop {
+
+	public static void main(String args[]) {
+		int x = 10;
+
+		do {
+			System.out.println("value of x : " + x);
+			x++;
+		} while (x < 15);
+	}
+
+}
---
+++ src/loops/ForLoop.java
@@ -0,0 +1,10 @@
+package loops;
+
+public class ForLoop {
+
+	public static void main(String args[]) {
+		for (int x = 10; x < 15; x = x + 1) {
+			System.out.println("value of x : " + x);
+		}
+	}
+}
