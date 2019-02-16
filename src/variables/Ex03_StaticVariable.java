dcd7444da2fe46fd7e0ee2201627ffb4835425b2
admin <dzunguyenit@gmail.com>
2019-02-16 15:45:58 +0700

    update

---
+++ src/variables/Ex03_StaticVariable.java
@@ -0,0 +1,17 @@
+package variables;
+
+public class Ex03_StaticVariable {
+	public static int age = 10;
+
+	public static void dogAge() {
+		age = age + 7;
+		System.out.println("Dog age is : " + age);
+	}
+
+	public static void main(String args[]) {
+		System.out.println("Dog age before: " + Ex03_StaticVariable.age);
+		System.out.println("Dog age after call method dogAge ");
+		Ex03_StaticVariable.dogAge();
+	}
+
+}
