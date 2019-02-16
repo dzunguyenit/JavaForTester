8691177e23fa8ab0eca71614749b8178afa7ee14
admin <dzunguyenit@gmail.com>
2019-02-16 16:34:34 +0700

    u

---
+++ src/operators/LogicOperators.java
@@ -0,0 +1,13 @@
+package operators;
+
+public class LogicOperators {
+
+	public static void main(String args[]) {
+		boolean a = true;
+		boolean b = false;
+
+		System.out.println("a && b = " + (a && b));
+		System.out.println("a || b = " + (a || b));
+		System.out.println("!(a && b) = " + !(a && b));
+	}
+}
