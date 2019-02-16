771be7e5cbaa39bf8ea191f4d05eb87928eaf7d8
admin <dzunguyenit@gmail.com>
2019-02-16 16:41:34 +0700

    u

---
+++ src/operators/AssignmentOperators.java
@@ -0,0 +1,28 @@
+package operators;
+
+public class AssignmentOperators {
+
+	public static void main(String args[]) {
+		int a = 10;
+		int b = 20;
+		int c = 0;
+
+		c = a + b;
+		System.out.println("c = a + b : " + c);
+
+		c += a;
+		System.out.println("c += a : " + c);
+
+		c -= a;
+		System.out.println("c -= a : " + c);
+
+		c *= a;
+		System.out.println("c *= a : " + c);
+
+		c /= a;
+		System.out.println("c /= a : " + c);
+
+		c %= a;
+		System.out.println("c %= a : " + c);
+	}
+}
