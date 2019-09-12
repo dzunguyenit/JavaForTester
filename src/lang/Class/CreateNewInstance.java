60453477bafdfa1a846a5ee6cbb7ca05b69089ef
vunht <vunht@VID-VUNHT-L.Vingroup.local>
2019-09-12 18:08:36 +0700

    123

---
+++ src/lang/Class/CreateNewInstance.java
@@ -0,0 +1,26 @@
+package lang.Class;
+
+import java.util.Date;
+
+public class CreateNewInstance {
+	@SuppressWarnings("rawtypes")
+	public static void main(String[] args) {
+
+		try {
+			// date object
+			Date d = new Date();
+			Class cls = d.getClass();
+			System.out.println("Time = " + d.toString());
+
+			/*
+			 * creates a new instance of the class represented by this Class object cls
+			 */
+			Object obj = cls.newInstance();
+			System.out.println("Time = " + obj);
+		} catch (InstantiationException e) {
+			System.out.println(e.toString());
+		} catch (IllegalAccessException e) {
+			System.out.println(e.toString());
+		}
+	}
+}
