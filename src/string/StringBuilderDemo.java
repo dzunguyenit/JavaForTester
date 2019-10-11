46153b8f18b010de33d3d08d947fb77b0780b0eb
vunht <vunht@VID-VUNHT-L.Vingroup.local>
2019-10-11 21:39:52 +0700

    Strinbuilder

---
+++ src/string/StringBuilderDemo.java
@@ -0,0 +1,14 @@
+package string;
+
+public class StringBuilderDemo {
+	public static void main(String args[]) {
+
+		StringBuilder text = new StringBuilder("I Love My Family ");
+
+		String chilrenText = "Chilren";
+		text.append(chilrenText);
+		System.out.println("So sanh chuoi = " + text);
+
+	}
+
+}
