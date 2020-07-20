13d951870888787419209221b9a93dbf7e97eff6
VuNguyen <v.vunht@vinid.net>
2020-07-20 17:51:02 +0700

    123

--- src/javaAnnotation/AccessHelloAnnotation.java
+++ src/javaAnnotation/AccessHelloAnnotation.java
@@ -6,14 +6,21 @@ public class AccessHelloAnnotation {
 	public static void main(String[] args) throws NoSuchMethodException, SecurityException {
 		HellAnnotationoImpliment hello = new HellAnnotationoImpliment();
 		System.out.println("Class is: " + hello.getClass());
-		Method method = hello.getClass().getMethod("sayHello");
+		Method methodSayHello = hello.getClass().getMethod("sayHello");
 
 		System.out.println("Method is: " + hello.getClass().getMethod("sayHello"));
 
-		HelloAnnotation annotation = method.getAnnotation(HelloAnnotation.class);
+		HelloAnnotation annotationSayHello = methodSayHello.getAnnotation(HelloAnnotation.class);
 
-		System.out.println("value is: " + annotation.value());
-		System.out.println("value is: " + annotation.value1());
+		System.out.println("value is: " + annotationSayHello.value());
+		System.out.println("value is: " + annotationSayHello.value1());
+
+		Method methodYeah = hello.getClass().getMethod("sayYeah");
+
+		HelloAnnotation annotationSayYeah = methodYeah.getAnnotation(HelloAnnotation.class);
+
+		System.out.println("value is: " + annotationSayYeah.value());
+		System.out.println("value is: " + annotationSayYeah.value1());
 	}
 
 }
--- src/javaAnnotation/HellAnnotationoImpliment.java
+++ src/javaAnnotation/HellAnnotationoImpliment.java
@@ -1,9 +1,14 @@
 package javaAnnotation;
 
 public class HellAnnotationoImpliment {
-	@HelloAnnotation(value1 = 10, value = 100)
+	@HelloAnnotation(value1 = 10, value = 0)
 	public void sayHello() {
-		System.out.println("hello annotation");
+		System.out.println("sayHello");
+	}
+
+	@HelloAnnotation(value1 = 9, value = 10)
+	public void sayYeah() {
+		System.out.println("sayYeah");
 	}
 
 }
